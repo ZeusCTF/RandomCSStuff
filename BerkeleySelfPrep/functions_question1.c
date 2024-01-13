bd8bf2c7c13e52a97b0bca6ac76daaa388ccae0e
ZeusCTF <55359988+ZeusCTF@users.noreply.github.com>
2024-01-13 10:38:10 -0500

    progress

--- BerkeleySelfPrep/functions_question1.c
+++ BerkeleySelfPrep/functions_question1.c
@@ -1,13 +1,27 @@
-//Q1: What happens in the following functions?
+//Q1: What is the output of the following program?
+#include <stdio.h>
 
-void swap1(int x, int y){
-    int tmp = x;
-    x = y;
-    y = tmp;
+#define SIZE 3
+
+int sum(int a1[], int a2[]) {
+    int sum = 0;
+
+    for (int i = 0; i < SIZE; i++) {
+        a1[i] += a2[i];
+        sum += a1[i];
+    }
+    return sum;
 }
 
-void swap2(int &x, int &y){
-    int tmp = *x;
-    *x = *y;
-    *y = tmp;
+int main() {
+    int array1[SIZE] = {1,2,3};
+    int array2[SIZE] = {1,2,3};
+
+    int x = sum(array1, array2);
+
+    for (int i = 0; i < SIZE; i++) {
+        printf("%d + %d", array1[i], array2[i]);
+        if (i < SIZE - 1) printf(" + ");
+    }
+    printf(" = %d\n", x);
 }

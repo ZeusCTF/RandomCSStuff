414e82d93d402ad6d7eedad9f95ca60a3d3aa339
ZeusCTF <55359988+ZeusCTF@users.noreply.github.com>
2024-01-14 13:15:02 -0500

    updates

--- BerkeleySelfPrep/functions_question1.c
+++ BerkeleySelfPrep/functions_question1.c
@@ -10,6 +10,11 @@ int sum(int a1[], int a2[]) {
         a1[i] += a2[i];
         sum += a1[i];
     }
+    /*
+    loop 1 -> a1[i] = 2, sum = 2
+    loop 2 -> a1[i] = 4, sum = 6
+    loop 3 -> a1[i] = 6, sum = 12
+    */
     return sum;
 }
 
@@ -18,10 +23,28 @@ int main() {
     int array2[SIZE] = {1,2,3};
 
     int x = sum(array1, array2);
+    //x contains 12
+
+    // Printing the entire array using a for loop
+    printf("Elements of the array: ");
+    for (int j = 0; j < SIZE; ++j) {
+        printf("%d ", array1[j]);
+    }
+    printf("\n");
+
+
+
 
     for (int i = 0; i < SIZE; i++) {
         printf("%d + %d", array1[i], array2[i]);
+        /*
+        loop 1 -> 2 + 1
+        loop 2 -> 4 + 2
+        loop 3 -> 6 + 3
+        */
         if (i < SIZE - 1) printf(" + ");
+        //checks of i is at the final value, if it is, then don't print a +
     }
     printf(" = %d\n", x);
+    //prints the sum value calculated earlier
 }

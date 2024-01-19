7363ec214e64dde392d8df468e0d4f8839f9723c
ZeusCTF <55359988+ZeusCTF@users.noreply.github.com>
2024-01-19 11:40:41 -0500

    progress

--- BerkeleySelfPrep/structures_question2.c
+++ BerkeleySelfPrep/structures_question2.c
@@ -6,8 +6,10 @@ int main() {
     char s[3];
     int i;
     } x;
+/*
+Defines a structure x, that has a character array and integer value
+*/
 
-    printf("%d\n", (char *)&x.i - x.s);
-    printf("%d\n", sizeof(x) - sizeof(x.s) - sizeof(x.i));
-
+    printf("%d\n", (char *)&x.i - x.s); // the cast here converts the address of x.i to a char pointer, then subtracts this from x.s.  This should be 4
+    printf("%d\n", sizeof(x) - sizeof(x.s) - sizeof(x.i)); // This subtracts the size of the structure, the char array, and the integer.  This should be 1
 }

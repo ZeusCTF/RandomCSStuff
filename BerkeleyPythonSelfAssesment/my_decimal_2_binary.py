d44fecd6acb946eae29bc857e98d7dc40e1e711d
ZeusCTF <55359988+ZeusCTF@users.noreply.github.com>
2024-01-22 14:27:52 -0500

    Delete BerkeleyPythonSelfAssesment directory

--- BerkeleyPythonSelfAssesment/my_decimal_2_binary.py
+++
@@ -1,9 +0,0 @@
-def main(num):
-    ans = ''
-    print('Decimal form', str(num))
-    while num > 0:
-        ans += str(num % 2)
-        num //= 2
-    print('Binary form:', ans)
-main(5)
-
--- BerkeleyPythonSelfAssesment/my_even_odd.py
+++
@@ -1,5 +0,0 @@
-def main(start, end):
-    for num in range(start, (end +1)):
-        output = "EVEN" if num % 2 == 0 else "ODD"
-        print(f"{num} {output}")
-main(1,10)

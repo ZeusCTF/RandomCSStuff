98f0fec50f0ed7ddc4edba973f90df681426d5ee
ZeusCTF <55359988+ZeusCTF@users.noreply.github.com>
2024-01-22 14:27:18 -0500

    progress

--- BerkeleySelfPrep/operators_additional_question2.c
+++ BerkeleySelfPrep/operators_additional_question2.c
@@ -1,9 +1,18 @@
 // Q2: What output does the following code produce?
+#include <stdio.h>
 
-void initArray() {
+int main() {
     int array[3];
     int i = 3, x = 3;
 
     while (--i && (array[i] = --x))
           printf("%d\n", array[i]);
-}
+};
+
+/*
+This code will output:
+2
+1
+
+This will happen because x will reach 0 before the last element is added to the array, breaking out of the loop
+*/
---
+++ BerkeleySelfPrep/operators_additional_question3.c
@@ -0,0 +1,10 @@
+// Q3: What do the following functions do?
+
+void counti(int n) {
+    while (n-- >= 0)
+    printf("%d\n", n);
+}
+void countu(unsigned n) {
+    while (n-- >= 0)
+    printf("%u\n", n);
+}

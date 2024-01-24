42a1ce61ab306ff8e2f6aca94ba1a7687ca74aa0
ZeusCTF <55359988+ZeusCTF@users.noreply.github.com>
2024-01-24 09:13:12 -0500

    progress

---
+++ BerkeleySelfPrep/focused_knowledge_question1.c
@@ -0,0 +1,25 @@
+// Q1: What is the output of the following program?
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main() {
+
+    char *t;
+    const char *str = "item1 item2 item3 item4 item5"; //declares a pointer to a constant string 'str' variable
+
+    char *tmp = (char *)malloc(strlen(str) + 1);
+    if (tmp == NULL) {
+        exit(1);
+    }
+    strcpy(tmp, str);  //copies str into tmp
+    t = strtok(tmp, " "); //splits a string based on a given delim.  So in this case, splits tmp based upon spaces
+    printf("%s,", t); //prints the result
+
+    while (t = strtok(0, " ")) {
+        printf("%s,", t);
+    }
+
+    free(tmp);
+    tmp = NULL;
+}
--- BerkeleySelfPrep/operators_additional_question3.c
+++ BerkeleySelfPrep/operators_additional_question3.c
@@ -1,10 +1,18 @@
 // Q3: What do the following functions do?
+#include <stdio.h>
+
+void countu(unsigned n) {
+    while (n-- >= 0)
+    printf("%u\n count u", n);
+}
 
 void counti(int n) {
     while (n-- >= 0)
     printf("%d\n", n);
 }
-void countu(unsigned n) {
-    while (n-- >= 0)
-    printf("%u\n", n);
-}
+
+/*
+Both functions perform the same general action, that being to decrement the n variable and print it to the screen as long as it is ge 0.
+The difference between the two is that counti prints the resulting value as an integer while countu prints the value as an unsigned int.
+But as an unsigned integer cannot be negative, this value will be represented as a very large positive integer > thus will continue to infinity.
+*/

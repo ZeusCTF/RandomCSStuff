67a89a2761af2ddbdbd79abe402201f0e8c419a2
ZeusCTF <55359988+ZeusCTF@users.noreply.github.com>
2024-02-01 17:38:59 -0500

    progress

--- LeetCode_Solutions/20.py
+++ LeetCode_Solutions/20.py
@@ -9,5 +9,8 @@ def main(s):
     for x in s:
         if x in bracket:
             stack.append(x)
-        
-main('()[]{}')
+        elif len(stack) > 0 and x == bracket[stack[-1]]:
+            stack.pop()
+        else:
+            return False
+    return not stack

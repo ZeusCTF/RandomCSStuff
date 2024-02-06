a5e94c2bdd61598ab2088d2deaabf3ca17eb55c2
ZeusCTF <55359988+ZeusCTF@users.noreply.github.com>
2024-02-06 09:28:48 -0500

    updates

--- LeetCode_Solutions/58.py
+++ LeetCode_Solutions/58.py
@@ -1,12 +1,24 @@
 def main(s):
     ans = 0
-    for letter in s[::-1]:
+
+    i = len(s) - 1
+    for letter in s[-1]:
+        while letter == " ":
+            print('space')
+        while letter != " ":
+            ans += 1
+        print(ans)
+        return
+    
+main('luffy is still joyboy    ')
+
+"""
+Thanks to built-in string methods, something like this works in Python and is much easier.  But...  I think an interviewer would prefer you to not use any built-in methods
+
+ans = 0
+    for letter in s.strip()[::-1]:
         if letter != ' ':
             ans += 1
         else:
             break
-    print(ans)
-        
-
-
-main('luffy is still joyboy')
+"""

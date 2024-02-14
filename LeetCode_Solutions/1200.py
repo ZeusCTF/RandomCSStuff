54a79f6050b5fbe4172c59b32e9a2e72a7004dfb
ZeusCTF <55359988+ZeusCTF@users.noreply.github.com>
2024-02-14 09:44:36 -0500

    progress

--- LeetCode_Solutions/1200.py
+++ LeetCode_Solutions/1200.py
@@ -1,4 +1,19 @@
 def main(arr):
-    
+    arr.sort()
+    min_dif = arr[1] - arr[0]
+    ans = []
+ 
+    while arr:
+        print(arr)
+        new_arr = []
+        if arr[1] - arr[0] == min_dif:
+            new_arr.append(arr[0])
+            new_arr.append(arr[1])
+            ans.append(new_arr)
+        arr.pop(1)
+        arr.pop(0)
+    print(ans)
 
-main([4,2,1,3])
+        
+
+main([1,3,6,10,15,20])

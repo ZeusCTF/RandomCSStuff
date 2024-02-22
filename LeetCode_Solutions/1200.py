218475b603ffc3b71248fc26d171720ef2662840
ZeusCTF <55359988+ZeusCTF@users.noreply.github.com>
2024-02-22 11:18:42 -0500

    progress

--- LeetCode_Solutions/1200.py
+++ LeetCode_Solutions/1200.py
@@ -3,18 +3,11 @@ def main(arr):
     ans = []
     min_dif = 99**99
     for i in range(1, len(arr)):
-        prev = 1 - i
+        prev = i - 1
         diff = abs(arr[i] - arr[prev])
         if diff < min_dif:
             min_dif = diff
             ans = [[arr[prev],arr[i]]]
         elif diff == min_dif:
-            ans.append(arr[prev])
-            ans.append(arr[i])
+            ans.append([arr[prev],arr[i]])
     print(ans)
-    
-        
-
-        
-
-main([3,8,-10,23,19,-4,-14,27])
---
+++ LeetCode_Solutions/509.py
@@ -0,0 +1,12 @@
+def main(n):
+    
+
+
+"""
+0 + 1 = 1
+1 + 1 = 2
+1 + 2 = 3
+2 + 3 = 5
+
+https://www.codechef.com/wiki/tutorial-dynamic-programming
+"""

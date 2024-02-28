13e0da6a681e9233bff82525dff61abf2944cbd5
ZeusCTF <55359988+ZeusCTF@users.noreply.github.com>
2024-02-28 10:40:38 -0500

    progress

--- LeetCode_Solutions/535.py
+++ LeetCode_Solutions/535.py
@@ -1,15 +1,15 @@
+import base64
 class Codec:
 
-    def encode(self, longUrl: str) -> str:
-        """Encodes a URL to a shortened URL.
-        """
+    def encode(self, longUrl):
+        longUrl = base64.b64encode(longUrl.encode()).decode()
+        print(longUrl.rstrip("="))
+
+
+    def decode(self, shortUrl):
+        pass
         
 
-    def decode(self, shortUrl: str) -> str:
-        """Decodes a shortened URL to its original URL.
-        """
-        
-
-# Your Codec object will be instantiated and called as such:
-# codec = Codec()
+codec = Codec()
+codec.encode("https://leetcode.com/problems/design-tinyurl")
 # codec.decode(codec.encode(url))

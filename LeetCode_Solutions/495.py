67b1499c905dbf812a8d62b9ffc7dc7408ef4d57
ZeusCTF <55359988+ZeusCTF@users.noreply.github.com>
2024-03-02 10:07:38 -0500

    progress

--- LeetCode_Solutions/495.py
+++ LeetCode_Solutions/495.py
@@ -2,10 +2,17 @@ def main(timeSeries, duration):
     l = len(timeSeries)
     total = 0
 
-    for occurance in range(l):
-        if timeSeries[occurance + 1]:
-            if timeSeries[occurance] + duration < timeSeries[occurance + 1]:
-                total += duration
+    for i in range(l):
+       if i < l - 1:
+           if timeSeries[i + 1] - timeSeries[i] >= duration:
+               total += duration
+           else:
+               total += timeSeries[i + 1] - timeSeries[i]
 
+       else:
+           total += duration
+    
     print(total)
+
+        
 main([1,4],2)

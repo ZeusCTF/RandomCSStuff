45005860611a83f3e0bd2540beb7f93cc6d8521d
ZeusCTF <55359988+ZeusCTF@users.noreply.github.com>
2024-03-12 15:56:32 -0400

    progress

--- LeetCode_Solutions/35.py
+++ LeetCode_Solutions/35.py
@@ -2,15 +2,24 @@ def main(nums, target):
     low = 0
     up = len(nums) - 1
 
-    while low <= up: 
-        mid = (low + up) // 2        
+    while low <= up:
+        mid = (low + up) // 2
         if nums[mid] == target:
-            print(nums.index(target))
-            return
+            print(mid)
+            return mid
         elif nums[mid] < target:
             low = mid + 1
         else:
             up = mid - 1
-    if low > up:
-        print(mid + 1)
-main([1,3,5,6], 7)
+
+    if low > up and target > nums[up]:
+        print(up + 1)
+        return up + 1
+    elif low > up and target < nums[low]:
+        print(low)
+        return low
+    else:
+        print(0)
+        return 0
+
+main([1,3], 2)

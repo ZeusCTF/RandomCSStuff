e5126bed9f2fbe8511a8157085b1ae85ae52f125
ZeusCTF <55359988+ZeusCTF@users.noreply.github.com>
2024-03-14 13:31:46 -0400

    progress

--- LeetCode_Solutions/67.py
+++ LeetCode_Solutions/67.py
@@ -2,10 +2,21 @@ def main(a,b):
     res = ""
     carry = 0
 
-    for i in range(max(len(a), len(b))):
-        digitA = a[i] if i < len(a) else 0
-        digitB = b[i] if i < len(b) else 0
+    a = a[::-1]
+    b = b[::-1]
 
-        print(digitA, digitB)
+    for i in range(max(len(a), len(b))):
+        digitA = ord(a[i]) - ord("0") if i < len(a) else 0
+        digitB = ord(b[i]) - ord("0") if i < len(b) else 0
+
+        total = digitA + digitB + carry
+        char = str(total % 2)
+        res = char + res
+        carry = total // 2
+
+    if carry:
+        res = "1" + res
+    print(res)
+
 
 main("11", "1")

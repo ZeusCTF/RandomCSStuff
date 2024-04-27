c7844d7901a9256e6e2fecad9b9f9069c6598b38
ZeusCTF <55359988+ZeusCTF@users.noreply.github.com>
2024-04-26 20:43:28 -0400

    progress

--- LeetCode_Solutions/832.py
+++ LeetCode_Solutions/832.py
@@ -1,11 +1,14 @@
 def main(image):
     for arr in image:
+        arr.reverse()
         for i in arr:
-            if i == 1:
-                i == 0
+            if arr[i] == 1:
+                arr[i] = 0
             else:
-                i == 1
+                arr[i] = 1
+    print(image)
+   
         
 
 
-main([[1,1,0],[1,0,1],[0,0,0]])
+main([[1,1,0], [0,1,0]])

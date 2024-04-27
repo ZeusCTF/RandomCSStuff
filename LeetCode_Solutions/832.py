1317866982cd4059a1ee0272a69b860da13d7268
ZeusCTF <55359988+ZeusCTF@users.noreply.github.com>
2024-04-27 15:40:50 -0400

    progress

--- LeetCode_Solutions/832.py
+++ LeetCode_Solutions/832.py
@@ -1,12 +1,23 @@
 def main(image):
     for arr in image:
         arr.reverse()
+        for ele in arr:
+            if ele == 1:
+               arr[ele] = 0
+               
+        print(arr)
+    print(image)
+
+
+
+
+    """
         for i in arr:
             if arr[i] == 1:
                 arr[i] = 0
-            else:
+            elif arr[i] == 0:
                 arr[i] = 1
-    print(image)
+    """
    
         
 

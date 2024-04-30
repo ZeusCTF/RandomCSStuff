c61a780f4f0f84f6890bccc47dd902cbe9e28b6f
ZeusCTF <55359988+ZeusCTF@users.noreply.github.com>
2024-04-30 12:01:24 -0400

    updates

--- LeetCode_Solutions/832.py
+++ LeetCode_Solutions/832.py
@@ -1,17 +1,28 @@
 def main(image):
+
     mid = round(len(image[0]) / 2)
+
+    
     
     for arr in image:
         count = -1
         for i in range(0, mid):
+
             last = arr[count]
-            arr[count] = arr[i]
+            first = arr[i]
+
+            if last == 1:
+                last = 0
+            else:
+                last = 1
+            
+            if first == 1:
+                first = 0
+            else:
+                first = 1
+            
+            arr[count] = first
             arr[i] = last
             count -= 1
+
     print(image)
-
-   
-        
-
-
-main([[1,1,0], [0,1,0]])

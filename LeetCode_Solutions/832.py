ceb1ebe65f846fae82a4d8932a9a18833193eefa
ZeusCTF <55359988+ZeusCTF@users.noreply.github.com>
2024-05-01 11:31:51 -0400

    losing it haha

--- LeetCode_Solutions/832.py
+++ LeetCode_Solutions/832.py
@@ -1,28 +1,19 @@
 def main(image):
+    if len(image[0]) > 1:
+        mid = round(len(image[0]) / 2)
 
-    mid = round(len(image[0]) / 2)
+        
+        
+        for arr in image:
+            for i in range(0, mid):
 
+                last = arr[~i]
+                first = arr[i]           
+                arr[~i] = abs(first - 1)
+                arr[i] = abs(last - 1)
+
+        print(image)
+    else:
+        print([[abs(image[0][0] - 1)]])
     
-    
-    for arr in image:
-        count = -1
-        for i in range(0, mid):
-
-            last = arr[count]
-            first = arr[i]
-
-            if last == 1:
-                last = 0
-            else:
-                last = 1
-            
-            if first == 1:
-                first = 0
-            else:
-                first = 1
-            
-            arr[count] = first
-            arr[i] = last
-            count -= 1
-
-    print(image)
+main([[1,1,0],[1,0,1],[0,0,0]])

1ef96026470905c8c8669e577e6cfea68137ce73
ZeusCTF <55359988+ZeusCTF@users.noreply.github.com>
2024-05-04 19:30:23 -0400

    progress

--- LeetCode_Solutions/867.py
+++ LeetCode_Solutions/867.py
@@ -1,19 +1,19 @@
 def main(matrix):
     ans = []
     x = len(matrix)
-    #print(x)
-    for i in range(0,x):
-        temp = []
-        count = 0
-        while count <= len(matrix):
-            print(count)
-            print('---')
-            print(i)
-            temp.append(matrix[count][i])
-            count += 1
-        ans.append(temp)
+    n = len(matrix[0])
+    for i in range(0, n):
+        app = []
+        for j in range(0,x):
+            app.append(matrix[j][i])
+        ans.append(app)
+    
     print(ans)
 
+
+
 main([[1,2,3],[4,5,6]])
+#[1,2,3],[4,5,6]
+
 #[1,2,3],[4,5,6],[7,8,9]
 #-> [1,4,7],[2,5,8],[3,6,9]

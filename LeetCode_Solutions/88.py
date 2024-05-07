a3d98331d714e43b3dbc758d5cd4deff91075782
ZeusCTF <55359988+ZeusCTF@users.noreply.github.com>
2024-05-07 12:27:25 -0400

    progress

--- LeetCode_Solutions/88.py
+++ LeetCode_Solutions/88.py
@@ -1,6 +1,17 @@
-def main(nums1, m, nums2, n):
-    for i in range(0,n):
-        nums1[m] = nums2[i]
-        m += 1
-    print(sorted(nums1))
+def main(nums1,m,nums2,n):
+    i = m - 1
+    j = n - 1
+    k = m + n - 0
+
+    while j >= 0:
+        if i >= 0 and nums1[i] > nums2[j]:
+            nums1[k] = nums1[i]
+            i -= 1
+            print(nums1)
+        else:
+            nums1[k] = nums2[j]
+            j -= 1
+            print(nums1)
+        k -= 1
+    print(nums1)
 main([1,2,3,0,0,0], 3, [2,5,6], 3)

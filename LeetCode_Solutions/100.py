1b4ee1bb1f7467495ffb73b465883431b1e5d9a3
ZeusCTF <55359988+ZeusCTF@users.noreply.github.com>
2024-05-09 10:04:00 -0400

    progress

--- LeetCode_Solutions/100.py
+++ LeetCode_Solutions/100.py
@@ -4,13 +4,13 @@ class TreeNode:
         self.val = val
         self.left = left
         self.right = right
-
-def check(node):
-    if not p and not q:
-        return True
-    if not p or not q:
-        return False
-    if p.val != q.val:
-        return False
-    
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p and not q:
+            return True
+        if not p or not q:
+            return False
+        if p.val != q.val:
+            return False
+        return (self.isSameTree(p.left, q.left) and self.isSameTree(p.left, q.left))
     

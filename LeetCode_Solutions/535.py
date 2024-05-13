a2fa71fc211dc0a219de9851cf4b5fe46250092f
ZeusCTF <55359988+ZeusCTF@users.noreply.github.com>
2024-05-13 15:23:31 -0400

    progress

--- LeetCode_Solutions/535.py
+++ LeetCode_Solutions/535.py
@@ -1,8 +1,21 @@
+import base64
+
 def encode(longurl):
     """Encodes a URL to a shortened URL.
     """
-    
+    enc = base64.b64encode(longurl.encode())
+    base = "https://leetcode/" + enc.decode()
+    print(base)
+    decode(base)
 
 def decode(url):
     """Decodes a shortened URL to its original URL.
     """
+    s = ""
+    for i in url[::-1]:
+        if i == '/':
+            s = s[::-1]
+            
+            return base64.b64decode(s).decode()
+        s += i
+encode("https://leetcode.com/problems/design-tinyurl")

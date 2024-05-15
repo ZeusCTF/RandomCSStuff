b9203227f8a6603ae4c1faa6216cfa93b7243614
ZeusCTF <55359988+ZeusCTF@users.noreply.github.com>
2024-05-14 21:12:17 -0400

    progress

--- LeetCode_Solutions/535.py
+++ LeetCode_Solutions/535.py
@@ -3,19 +3,23 @@ import base64
 def encode(longurl):
     """Encodes a URL to a shortened URL.
     """
-    enc = base64.b64encode(longurl.encode())
+    enc = base64.b16encode(longurl.encode())
     base = "https://leetcode/" + enc.decode()
-    print(base)
-    decode(base)
+   # print(base)
+    decode(base, enc)
 
-def decode(url):
+def decode(url, enc):
     """Decodes a shortened URL to its original URL.
     """
+    print(enc)
+    print(url)
     s = ""
     for i in url[::-1]:
         if i == '/':
             s = s[::-1]
             
-            return base64.b64decode(s).decode()
+            print(base64.b16decode(s).decode())
+            return base64.b16decode(s).decode()
         s += i
-encode("https://leetcode.com/problems/design-tinyurl")
+        
+encode("http://badge.example.net/beginner.aspx?aftermath=achiever&actor=air")

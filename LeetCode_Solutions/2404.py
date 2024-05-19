aa4017e56f4d6467c327aca61ba0bc6ff39b2887
ZeusCTF <55359988+ZeusCTF@users.noreply.github.com>
2024-05-19 11:40:49 -0400

    progress

--- LeetCode_Solutions/2404.py
+++ LeetCode_Solutions/2404.py
@@ -1,19 +1,23 @@
 
 def main(nums):
-    max = -1
+    maximum = -1
     even_nums = {}
+    prev_value = 0
+    prev_key = max(nums)
+
     for num in nums:
         if num % 2 == 0:
             even_nums[num] = 1 + even_nums.get(num, 0)
     if even_nums:
         for key, value in even_nums.items():
-            print(key, value)
-            print('-' * 50)
-            print(value, max)
-            print()
-            print()
-            if value > max:
-                max = key
-    print(max)
-    return max
+            
+            if value > prev_value:
+                prev_value = value
+                maximum = key
+            if value == prev_value and key < prev_key:
+                maximum = key
+                prev_key = key
+
+
+    return maximum
 main([0,1,2,0,0,0,2,4,4,1])

2610c4c6393e51956694ab3b71fbb38e9c5749a0
ZeusCTF <55359988+ZeusCTF@users.noreply.github.com>
2024-05-21 10:53:12 -0400

    progress

--- LeetCode_Solutions/468.py
+++ LeetCode_Solutions/468.py
@@ -1,11 +1,24 @@
 def main(queryIP):
 
     def ipv4(IP):
+        IP += '.'
         s = ''
-        count = 0
         for byte in IP:
+            if byte != '.':
+                s += byte
+            else:
+                count += 1
+                if s.isdigit() and int(s) >= 0 and int(s) < 255:
+                    print('valid octet')
+                    print(s)
+                    s = ''
+                else:
+                    print('wack')
+                    return False
+        
             
-
+    def ipv6(IP):
+        pass
 
 
     if '.' in queryIP:
@@ -14,4 +27,14 @@ def main(queryIP):
         ipv6(queryIP)
 
 
-main('127.0.0.1')
+#main('12c.0.0.1')
+
+def test(ip):
+    ip = ip.split('.')
+    if len(ip) != 4:
+        return False
+    for octet in ip:
+        if octet.isdigit() and octet >= 0 and octet < 255:
+
+
+test("192.168.1.1")

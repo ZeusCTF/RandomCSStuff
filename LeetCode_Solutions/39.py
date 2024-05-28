25e8f58e7eb6522564bc46a7a0a52251429edf73
ZeusCTF <55359988+ZeusCTF@users.noreply.github.com>
2024-05-28 10:39:22 -0400

    progress

--- LeetCode_Solutions/39.py
+++ LeetCode_Solutions/39.py
@@ -1,9 +1,20 @@
 def main(candidates, target):
     ans = []
 
-    for num in candidates:
-        if target % num == 0:
-            ans.append([num] * int(target / num))
-    print(ans)
+    def dfs(i, cur, total):
+        if total == target:
+            ans.append(cur.copy())
+            return
+        if i >= len(candidates) or total > target:
+            return
+        
+        cur .append(candidates[i])
+        dfs(i, cur, total + candidates[i])
+        cur.pop()
+        dfs(i + 1, cur, total)
+    
+    dfs(0, [], 0)
+    return ans
+
 
 main([2,3,6,7], 7)

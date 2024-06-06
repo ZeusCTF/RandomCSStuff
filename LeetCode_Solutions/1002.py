c3b7e23330d6dbc66cd22f3e679bd43e029b528c
ZeusCTF <55359988+ZeusCTF@users.noreply.github.com>
2024-06-06 11:45:19 -0400

    progress

--- LeetCode_Solutions/1002.py
+++ LeetCode_Solutions/1002.py
@@ -1,7 +1,11 @@
 def main(words):
+    d = {}
+
     for char in range(ord('a'), ord('z') + 1):
+        char = chr(char)
         for word in words:
             if char in word:
-                print(word, char)
-        
+                d[char] = d.get(char, 0) + word.count(char)
+    print(d)
+
 main(["bella","label","roller"])

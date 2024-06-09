14f735bc9aaf387d4f964a67c81a40553e693fef
ZeusCTF <55359988+ZeusCTF@users.noreply.github.com>
2024-06-09 13:15:37 -0400

    progress

--- LeetCode_Solutions/2161.py
+++ LeetCode_Solutions/2161.py
@@ -1,14 +1,18 @@
 def main(nums, pivot):
-    res = []
-    nums.sort()
-    #print(nums)
+    res = [pivot]
+    count = 0
+
     for num in nums:
         if num < pivot:
-            res.insert(0, num)
-        elif num == pivot:
+            res.insert(count, num)
+            count += 1
+        elif num > pivot:
             res.append(num)
         else:
-            res.append(num)
+            res.insert((res.index(pivot) + 1), num)
+    res.remove(pivot)
     print(res)
 
-main([-3,4,3,2], 2)
+main([9,12,5,10,14,3,10], 10)
+
+#quick note, I am VERY surpised this did not time out LMAO

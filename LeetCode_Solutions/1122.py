f297d40dffe161367dfaa47781e7239407e07cf6
ZeusCTF <55359988+ZeusCTF@users.noreply.github.com>
2024-06-14 15:33:24 -0400

    progress

--- LeetCode_Solutions/1122.py
+++ LeetCode_Solutions/1122.py
@@ -1,20 +1,22 @@
 def main(arr1, arr2):
-    res = []
+    result = []
+    
+    for i in range(len(arr2)):
+        for j in range(len(arr1)):
+            if arr1[j] == arr2[i]:
+                result.append(arr1[j])
+                arr1[j] = -1
+    
+    arr1.sort()
+    
+    for num in arr1:
+        if num != -1:
+            result.append(num)
+            
+    return result
 
-    d = {}
-    for item in arr2:
-        d[item] = arr2.index(item)
 
-    sorted(arr1)
-
-    for item in arr1:
-        if item in d:
-            print(d[item])
-            res.insert(d[item], item)
-        else:
-            res.append(item)
-
-    print(res)
+   
 
 
 main([28,6,22,8,44,17], [22,28,8,6])

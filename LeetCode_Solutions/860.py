0d2bd04c40e4ab2ed7a0141e2e824207bfeb1ffb
ZeusCTF <55359988+ZeusCTF@users.noreply.github.com>
2024-06-30 11:10:20 -0400

    progress: /

--- LeetCode_Solutions/860.py
+++ LeetCode_Solutions/860.py
@@ -1,3 +1,27 @@
 def main(bills):
+    fives = 0
+    tens = 0
+    twentys = 0
 
-main([5,5,5,10,20])
+    for bill in bills:
+        if bill == 5:
+            fives += 1
+        elif bill == 10:
+            if fives:
+                fives -= 1
+            else:
+                return False
+            tens += 1
+        else:
+            if tens and fives:
+                tens -= 1
+                fives -= 1
+            elif fives >= 3 and not tens:
+                fives -= 3
+            else:
+                return False
+            twentys += 1
+    return True
+
+
+print(main([5,5,10,20,5,5,5,5,5,5,5,5,5,10,5,5,20,5,20,5]))

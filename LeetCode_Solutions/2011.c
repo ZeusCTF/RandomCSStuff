9edd71dba841d2025aeab690df724ea2e1be8fc5
ZeusCTF <55359988+ZeusCTF@users.noreply.github.com>
2024-07-18 09:44:10 -0400

    progress

---
+++ LeetCode_Solutions/2011.c
@@ -0,0 +1,11 @@
+int finalValueAfterOperations(char** operations, int operationsSize) {
+    int res = 0;
+    char str1[] = "X++";
+    char str2[] = "++X";
+
+    for (int i = 0; i < operationsSize; i++){
+        if () {
+            res++;
+        }
+    } return res;
+}

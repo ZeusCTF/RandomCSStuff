7a26f3d54dd39a968eb3da2512a237c3b4998d86
ZeusCTF <55359988+ZeusCTF@users.noreply.github.com>
2024-07-25 09:45:44 -0400

    progress

--- LeetCode_Solutions/1431.c
+++ LeetCode_Solutions/1431.c
@@ -1,12 +1,16 @@
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
-kidsWithCandies(int* candies, int candiesSize, int extraCandies, int* returnSize) {
+#include <stdlib.h>
+#include <stdbool.h>
+
+bool* kidsWithCandies(int* candies, int candiesSize, int extraCandies, int* returnSize) {
 
     int res = 0;
     char* t = "true";
     char* f = "false";
-    int array[candiesSize];
+    bool array[candiesSize];
+    bool* result = (bool*)malloc(candiesSize * sizeof(bool));
     
   
     for (int i = 0; i < candiesSize; i++) { 

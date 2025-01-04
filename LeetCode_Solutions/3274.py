6ee00b0ebdd59b36ac4239bca1ef06eb8be03968
ZeusCTF <55359988+ZeusCTF@users.noreply.github.com>
2025-01-04 11:16:06 -0500

    2025

--- LeetCode_Solutions/3274.py
+++ LeetCode_Solutions/3274.py
@@ -1,20 +1,24 @@
 def checkTwoChessboards(coordinate1, coordinate2):
-    if int(ord(coordinate1[0])) % 2 != 0 and int(coordinate1[1]) % 2 != 0:
-        coordinate1 = 'black'
-    else:
-        coordinate1 = 'white'
+    def cord_check(coord):
+        first = int(ord(coord[0]))
+        second = int(coord[1])
 
-    if int(ord(coordinate2[0])) % 2 != 0 and int(coordinate2[1]) % 2 != 0:
-        coordinate2 = 'black'
-    else:
-        coordinate2 = 'white'
+        if first % 2 != 0 and second % 2 != 0:
+            coord = 'black'
+            return coord
+        elif first % 2 == 0 and second % 2 == 0:
+            coord = 'black'
+            return coord
+        else:
+            coord = 'white'
+            return coord
+
+    coordinate1 = cord_check(coordinate1)
+    coordinate2 = cord_check(coordinate2)
 
     if coordinate2 == coordinate1:
-        print('same')
         return True
     else:
-        print('not')
         return False
 
-checkTwoChessboards('a1','c3')
-        
+checkTwoChessboards('a7','a6')

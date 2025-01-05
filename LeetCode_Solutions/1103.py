ebae54ef929a1ffa6242a5aa94070c3eb7d62e60
ZeusCTF <55359988+ZeusCTF@users.noreply.github.com>
2025-01-05 18:30:57 -0500

    2025

---
+++ LeetCode_Solutions/1103.py
@@ -0,0 +1,17 @@
+def distributeCandies(candies, num_people):
+    res = []
+    reverse = candies - 1
+    while candies:
+        
+        
+
+
+distributeCandies(7, 4) #[1,2,3,1]
+
+
+"""
+[1,0,0,0]
+[1,2,0,0]
+[1,2,3,0]
+[1,2,3,1]
+"""
